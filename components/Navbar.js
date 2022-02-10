f247c94b1f15f83fedff80fbe4d186b328a7d5b7
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-10 22:24:21 +0530

    Navbar skeleton

---
+++ components/Navbar.js
@@ -0,0 +1,28 @@
+import { BsFillChatFill } from 'react-icons/bs'
+import styles from './navbar.module.css'
+
+export default function Navbar() {
+	return (
+		<nav className={styles['navbar']}>
+			<img src='/images/logo.svg' height={32} width='auto' className={styles['logo']} />
+			<ul className={styles['links-list']}>
+				<li>
+					<a>About us</a>
+				</li>
+				<li>
+					<a>Benefits</a>
+				</li>
+				<li>
+					<a>Pricing</a>
+				</li>
+				<li>
+					<a>FAQs</a>
+				</li>
+			</ul>
+			<button className={styles['talk-button']}>
+				<BsFillChatFill />
+				<span>Talk to us</span>
+			</button>
+		</nav>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Navbar from '../components/Navbar'
+
 export default function Home() {
 	return (
 		<div>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<Navbar />
 		</div>
 	)
 }
