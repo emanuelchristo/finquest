f8cf38824bdd8ed9ba9ef95a396ed01d403f3fa2
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-12 17:26:03 +0530

    Associated component skeleton

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import Associated from '../components/Associated'
 import Header from '../components/Header'
 
 export default function Home() {
 	return (
 		<div>
 			<Header />
+			<Associated />
 		</div>
 	)
 }
