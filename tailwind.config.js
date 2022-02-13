4925924e4d3c76fd5982316749a638d0e77588f4
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-13 21:39:59 +0530

    tailwing setup for desktop first, media-queries.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@ module.exports = {
 	content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
 	theme: {
 		extend: {},
+		screens: {
+			'2xl': { max: '1536px' },
+			// => @media (max-width: 1535px) { ... }
+
+			xl: { max: '1280px' },
+			// => @media (max-width: 1279px) { ... }
+
+			lg: { max: '1024px' },
+			// => @media (max-width: 1023px) { ... }
+
+			md: { max: '768px' },
+			// => @media (max-width: 767px) { ... }
+
+			sm: { max: '640px' },
+			// => @media (max-width: 639px) { ... }
+		},
 	},
 	plugins: [],
 }
