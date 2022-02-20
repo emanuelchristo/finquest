0b6ed48fabcc0975bba5ff7b32f3befe444b1644
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-20 13:59:53 +0530

    consultation responsive

--- components/Consultation.js
+++ components/Consultation.js
@@ -5,12 +5,12 @@ export default function Consultation() {
 	return (
 		<div className='margin'>
 			<section className={styles['consultation']}>
-				<img src='/images/consultation.png' alt='' width='440px' height='auto' />
+				<img className={styles['image']} src='/images/consultation.png' alt='' width='440px' height='auto' />
 				<div className={styles['spacer']}></div>
 				<div className={styles['content']}>
 					<h2>Consultation</h2>
 					<p>Chat with us on WhatsApp or schedule a free 30-minutes call with our team</p>
-					<div className='flex items-center'>
+					<div className='flex items-center flex-wrap-reverse'>
 						<a className={styles['phone-no']} href='tel:+918075145434'>
 							+91 80751 45434
 						</a>
