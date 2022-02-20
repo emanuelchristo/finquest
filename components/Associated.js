5cf4df36168ecb6a4a7f2d347d4a3f8604db51d7
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-20 12:54:11 +0530

    reviews section responsive

--- components/Associated.js
+++ components/Associated.js
@@ -2,10 +2,12 @@ import styles from './associated.module.css'
 
 export default function Associated() {
 	return (
-		<div className={styles['associated']}>
-			<img src='/images/upstox.svg' alt='Upstox' height='38px' width='auto' />
-			<img src='/images/trading-view.svg' alt='Trading View' height='38px' width='auto' />
-			<img src='/images/zerodha.svg' alt='Zerodha' height='38px' width='auto' />
+		<div className='margin'>
+			<div className={styles['associated']}>
+				<img src='/images/upstox.svg' alt='Upstox' height='38px' width='auto' />
+				<img src='/images/trading-view.svg' alt='Trading View' height='38px' width='auto' />
+				<img src='/images/zerodha.svg' alt='Zerodha' height='38px' width='auto' />
+			</div>
 		</div>
 	)
 }
--- components/Consultation.js
+++ components/Consultation.js
@@ -3,22 +3,24 @@ import styles from './consultation.module.css'
 
 export default function Consultation() {
 	return (
-		<section className={styles['consultation']}>
-			<img src='/images/consultation.png' alt='' width='440px' height='auto' />
-			<div className={styles['spacer']}></div>
-			<div className={styles['content']}>
-				<h2>Consultation</h2>
-				<p>Chat with us on WhatsApp or schedule a free 30-minutes call with our team</p>
-				<div className='flex items-center'>
-					<a className={styles['phone-no']} href='tel:+918075145434'>
-						+91 80751 45434
-					</a>
-					<button className={styles['whatsapp-button']}>
-						<ImWhatsapp className={styles['whatsapp-icon']} />
-						<span>WhatsApp</span>
-					</button>
+		<div className='margin'>
+			<section className={styles['consultation']}>
+				<img src='/images/consultation.png' alt='' width='440px' height='auto' />
+				<div className={styles['spacer']}></div>
+				<div className={styles['content']}>
+					<h2>Consultation</h2>
+					<p>Chat with us on WhatsApp or schedule a free 30-minutes call with our team</p>
+					<div className='flex items-center'>
+						<a className={styles['phone-no']} href='tel:+918075145434'>
+							+91 80751 45434
+						</a>
+						<button className={styles['whatsapp-button']}>
+							<ImWhatsapp className={styles['whatsapp-icon']} />
+							<span>WhatsApp</span>
+						</button>
+					</div>
 				</div>
-			</div>
-		</section>
+			</section>
+		</div>
 	)
 }
