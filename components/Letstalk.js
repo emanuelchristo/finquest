1706ffd948f286453a10d3638bce1a900c37ba9b
emanuelchristo <emanuelchristochris@gmail.com>
2022-02-28 22:29:08 +0530

    lets talk polish

--- components/Letstalk.js
+++ components/Letstalk.js
@@ -1,54 +1,59 @@
-
 import styles from './letstalk.module.css'
 import { HiPhone } from 'react-icons/hi'
 import { MdMail } from 'react-icons/md'
 
-const icons = [{
-    img : '/icons/letstalk-1.png'
-},
-{
-    img : '/icons/letstalk-2.png'
-},
-{
-    img : '/icons/letstalk-3.png'
-}]
+const icons = [
+	{
+		img: '/icons/instagram.svg',
+	},
+	{
+		img: '/icons/twitter.svg',
+	},
+	{
+		img: '/icons/linkedin.svg',
+	},
+]
 
 export default function Letstalk() {
-
-
-    return (
-        <div className="margin">
-
-            <section className={styles['letstalk']}>
-                <h2>Let's Talk</h2>
-                <div className={styles['content-wrapper']}>
-                    <div className={styles['letstalk-text']}>
-                        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. ut quaerat deserunt et soluta provident eum molestias vitae!</p>
-                    </div>
-                    <div className={styles['letstalk-contact']}>
-                        <div className={styles['contact-div']}>
-                            <HiPhone /> 
-                            +91 80751 45434
-                        </div>
-                        <div className={styles['contact-div']}>
-                            <HiPhone />
-                            +91 73562 66418
-                        </div>
-                        <div className={styles['contact-div2']}>
-<MdMail />                            hellofinquest@gmail.com
-                        </div> 
-                      
-                    </div>
-                    <div className={styles['contact-icons-wrap']}>
-                            {icons.map((icon)=>(
-                                <div className={styles['letstalk-icon']}>
-                                    <img src={icon.img} alt="" />
-                                </div>
-                            ))}
-                        </div>
-                       
-                </div>
-            </section>
-        </div>
-    )
+	return (
+		<div className='margin'>
+			<section className={styles['letstalk']}>
+				<div className={styles['heading-wrapper']}>
+					<img
+						className={styles['decoration']}
+						src='/images/lets-talk-decoration.svg'
+						width='60px'
+						height='auto'
+						alt=''
+					/>
+					<h2>Let&apos;s talk</h2>
+				</div>
+				<div className={styles['content-wrapper']}>
+					<div className={styles['letstalk-text']}>
+						<p>A nunc lacus nisl, neque, quam. Vitae leo</p>
+					</div>
+					<div className={styles['letstalk-contact']}>
+						<div className={styles['contact-div']}>
+							<HiPhone />
+							<span>+91 80751 45434</span>
+						</div>
+						<div className={styles['contact-div']}>
+							<HiPhone />
+							<span>+91 73562 66418</span>
+						</div>
+						<div className={styles['contact-div2']}>
+							<MdMail /> <span>hellofinquest@gmail.com</span>
+						</div>
+					</div>
+					<div className={styles['contact-icons-wrap']}>
+						{icons.map((icon, index) => (
+							<div key={index} className={styles['letstalk-icon']}>
+								<img src={icon.img} alt='' />
+							</div>
+						))}
+					</div>
+				</div>
+			</section>
+		</div>
+	)
 }
