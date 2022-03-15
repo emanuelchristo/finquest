43d509b16ce7b5bb74c53a631b1c6b8bb07ade76
emanuelchristo <emanuelchristochris@gmail.com>
2022-03-15 17:12:41 +0530

    snackbar space

--- components/SnackBar.js
+++ components/SnackBar.js
@@ -6,16 +6,18 @@ import styles from './snackbar.module.css'
 export default function Snackbar(props) {
 	const [hidden, setHidden] = useState(false)
 	return (
-		<div className={styles['main']} style={{ display: hidden ? 'none' : 'block' }}>
-			<p className={styles['snack-text']}>{props.message} </p>
-			<button
-				className={styles['snack-close']}
-				onClick={(e) => {
-					setHidden(!hidden)
-				}}
-			>
-				<IoMdClose />
-			</button>
+		<div className={styles['wrapper']} style={{ display: hidden ? 'none' : 'block' }}>
+			<div className={styles['main']}>
+				<p className={styles['snack-text']}>{props.message} </p>
+				<button
+					className={styles['snack-close']}
+					onClick={(e) => {
+						setHidden(!hidden)
+					}}
+				>
+					<IoMdClose />
+				</button>
+			</div>
 		</div>
 	)
 }
