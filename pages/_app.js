e3d617e186b04878e0b9d305aa260d9b278aeba5
n48331 <unfolks@gmail.com>
2022-04-21 19:14:21 +0530

    Navbar margins

--- pages/_app.js
+++ pages/_app.js
@@ -12,9 +12,9 @@ function MyApp({ Component, pageProps }) {
 				<title>Finquest - Start learning how to invest &amp; trade in stock market easily.</title>
 				<link rel='icon' type='image/png' href='/favicon.png'></link>
 			</Head>
+			<Snackbar message=' 🎉  Flat ₹2499 off for first 50 people!' />
 			<Component {...pageProps} />
 
-			<Snackbar message=' 🎉  Flat ₹2499 off for first 50 people!' />
 			<Footer />
 
 		</div>
--- pages/index.js
+++ pages/index.js
@@ -18,6 +18,7 @@ export default function Home() {
 	return (
 		<div id='home'>
 			<Header />
+
 			<Associated />
 			<div className='section-spacer'></div>
 			<About />
