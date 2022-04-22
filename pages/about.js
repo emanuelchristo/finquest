de1ba98fca0b682b05b0345a3aca313c879151e7
n48331 <unfolks@gmail.com>
2022-04-22 17:36:50 +0530

    Nav space

--- pages/about.js
+++ pages/about.js
@@ -1,18 +1,20 @@
 
 import About1 from '../components/aboutUs/about1'
 import About2 from '../components/aboutUs/about2'
-import WhyUs2 from '../components/aboutUs/WhyUs1'
-import WhyUs3 from '../components/aboutUs/WhyUs2'
+import WhyUs1 from '../components/aboutUs/WhyUs1'
+import WhyUs2 from '../components/aboutUs/WhyUs2'
 import Navbar from '../components/Navbar'
+import styles from './styles.module.css'
 
 export default function AboutUs() {
     return (
-        <div>
+        <div id='home'>
+            <div id='home' className={styles['nav-spacer']}></div>
             <Navbar />
             <About1 />
             <About2 />
+            <WhyUs1 />
             <WhyUs2 />
-            <WhyUs3 />
 
 
         </div>
