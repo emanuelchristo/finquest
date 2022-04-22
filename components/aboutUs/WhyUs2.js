1d63b7cec54cef9bdd3648a4ef8b79de14d3d899
n48331 <unfolks@gmail.com>
2022-04-22 17:08:03 +0530

    About us page responsivve

--- components/aboutUs/WhyUs2.js
+++ components/aboutUs/WhyUs2.js
@@ -1,26 +1,24 @@
-import styles from './whyus2.module.css'
-import WhyUs1 from './WhyUs1'
-export default function WhyUs2() {
+import styles from './whyus.module.css'
+
+export default function WhyUs3() {
     return (
         <div id='about' className='margin'>
-            <WhyUs1 />
-            <section className={styles['about']}>
-
-                <div className={styles['content']}>
+            <section className={styles['about2']}>
+                <img className={styles['image']} src='/images/whyus3.png' alt='' width='50%' height='auto' />
+                <div className={styles['spacer']}></div>
+                <div className={styles['content2']}>
                     <div className={styles['heading-wrapper']}>
 
                         {/* <h2>About us</h2> */}
                     </div>
                     <p>
-                        Collaboration and support are the keys to success. Learning is a lifelong
-                        endeavor. We are, nevertheless, the finest at delivering everything we
-                        recommend. Community-based training is a hidden gem that gives you a lot
-                        more than just basic skills when it comes to stock trading. Cosmic Wealth
-                        is a network that allows you to trade alongside the greatest in the country,
-                        ensuring that you never fall behind the times.
+                        The possibilities are boundless, and the interaction even more so,
+                        with several, dedicated view-sharing platforms for segments including
+                        equities, commodities, and international markets. What distinguishes us
+                        from the competition? We consider all of your questions and feedback,
+                        and we will always be there for you, whether you win or lose.
                     </p>
                 </div>
-                <img className={styles['image']} src='/images/whyus2.png' alt='' width='50%' height='auto' />
 
             </section>
 
--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,8 @@
 
 import About1 from '../components/aboutUs/about1'
 import About2 from '../components/aboutUs/about2'
-import WhyUs1 from '../components/aboutUs/WhyUs1'
-import WhyUs2 from '../components/aboutUs/WhyUs2'
-import WhyUs3 from '../components/aboutUs/WhyUs3'
+import WhyUs2 from '../components/aboutUs/WhyUs1'
+import WhyUs3 from '../components/aboutUs/WhyUs2'
 import Navbar from '../components/Navbar'
 
 export default function AboutUs() {
