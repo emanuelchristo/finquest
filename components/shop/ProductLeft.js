9230a386c93f1c45e7d8e13601a8194df3950894
n48331 <unfolks@gmail.com>
2022-05-23 12:42:00 +0530

    shop

--- components/shop/ProductLeft.js
+++ components/shop/ProductLeft.js
@@ -10,36 +10,38 @@ export default function ProductLeft({ imageSrc, title, link, price }) {
 
             <section className={styles['aboutL']}>
                 <div className={styles['dt-sc-hr']}></div>
-                <div className={styles['contentL']}>
-                    <div className={styles['heading-wrapper']}>
-                    </div>
+                <div>
+                    <div className={styles['contentL']}>
+                        <div className={styles['heading-wrapper']}>
+                        </div>
 
 
-                    <Link href={`${link}`}>
-                        <a aria-label={title} className={styles['action-link']} rel='noreferrer noopener'>
-                            <motion.button
-                                whileHover={{ scale: 1.05 }}
-                                whileTap={{ scale: 0.95 }}
-                                className={styles['action-button']}
-                            >
-                                Buy Now
-                            </motion.button>
-                        </a></Link>
-                    {/* <p>{desc}
+                        <Link href={`${link}`}>
+                            <a aria-label={title} className={styles['action-link']} rel='noreferrer noopener'>
+                                <motion.button
+                                    whileHover={{ scale: 1.05 }}
+                                    whileTap={{ scale: 0.95 }}
+                                    className={styles['action-button']}
+                                >
+                                    Buy Now
+                                </motion.button>
+                            </a></Link>
+                        {/* <p>{desc}
                     </p> */}
-                    <div className="flex items-end gap-2">
-                        <span className="text-gray-800 text-xl md:text-4xl sm:text-4xl xsm:text-4xl font-bold">
-                            ₹{price}
-                        </span>
-                        <span className="text-red-500 text-xl line-through mb-0.5">₹{price * 1.25}</span>
-                        <span className="text-red-500 text-xl mb-0.5"> (-{25}%)</span>
-                    </div><br />
+                        <div className="flex items-end gap-2">
+                            <span className="text-gray-800 text-xl md:text-4xl sm:text-4xl xsm:text-4xl font-bold">
+                                ₹{price}
+                            </span>
+                            <span className="text-red-500 text-xl line-through mb-0.5">₹{price * 1.25}</span>
+                            <span className="text-red-500 text-xl mb-0.5"> (-{25}%)</span>
+                        </div><br />
 
-                    <h3>{title}</h3>
-                </div>
+                        <h3>{title}</h3>
+                    </div></div>
                 <div>
                     <img className={styles['image']} src={imageSrc} alt={title} width='110%' height='auto' />
                 </div>
+
             </section>
 
         </>
