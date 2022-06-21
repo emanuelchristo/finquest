5ed757e7a794dbe28c4fbffd70a8c6d52459e0b8
emanuelchristo <emanuelchristochris@gmail.com>
2022-06-21 21:18:48 +0530

    hiring text margin fix

--- components/aboutUs/Hiring.js
+++ components/aboutUs/Hiring.js
@@ -8,17 +8,11 @@ export default function Hiring() {
 		<div className='margin' id='community'>
 			<section className={styles['community']}>
 				<img src='/images/suitcase.svg' className={styles['suitcase']} />
-				<span className={styles['text']} >WORK AT FINQUEST</span>
-				<h2>
-					Let&apos;s work together
-
-				</h2>
-				<p>
-					We&apos;re looking for people who want make stock market trading effective
-				</p>
+				<span className={styles['text']}>WORK AT FINQUEST</span>
+				<h2>Let&apos;s work together</h2>
+				<p>We&apos;re looking for people who want make stock market trading effective</p>
 				<div className={styles['feature']}>
 					<a href={waLink('I would like to work at FINQUEST')} target='_blank' rel='noreferrer noopener'>
-
 						<motion.button
 							whileHover={{ scale: 1.05 }}
 							whileTap={{ scale: 0.95 }}
@@ -27,21 +21,10 @@ export default function Hiring() {
 							<MdMail className={styles['community-icon']} />
 							<span className={styles['join']}>Join us</span>
 						</motion.button>
-					</a></div>
-				<img
-					className={styles['arrow-1']}
-					src='/images/arrow-1.svg'
-					width='140px'
-					height='auto'
-					alt=''
-				/>
-				<img
-					className={styles['arrow-2']}
-					src='/images/arrow-2.svg'
-					width='140px'
-					height='auto'
-					alt=''
-				/>
+					</a>
+				</div>
+				<img className={styles['arrow-1']} src='/images/arrow-1.svg' width='140px' height='auto' alt='' />
+				<img className={styles['arrow-2']} src='/images/arrow-2.svg' width='140px' height='auto' alt='' />
 			</section>
 		</div>
 	)
