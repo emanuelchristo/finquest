fb972bfad00bfdcaff2564d3c8cdcf3893c6c199
n48331 <unfolks@gmail.com>
2022-07-15 23:02:21 +0530

    store-section

--- components/Store.js
+++ components/Store.js
@@ -6,11 +6,15 @@ export default function Store() {
 	return (
 		<div className='margin' id='shop'>
 			<section className={styles['store']}>
-				<img className={styles['image']} src='/images/store.png' alt='' width='440px' height='auto' />
+
+				<img className={styles['image']} src='/images/store.svg' alt='' width='440px' height='auto' />
 				<div className={styles['spacer']}></div>
 				<div className={styles['content']}>
-					<span className={styles['intro']}>INTRODUCING</span>
-					<h2>Finshop</h2>
+					<div className={styles['heading-wrapper']}>
+						<img className={styles['store-decor']} src='/images/finshop-decor.svg' alt='' />
+						<span className={styles['intro']}>INTRODUCING</span>
+						<h2>Finshop</h2>
+					</div>
 					<p>Our premium trader-wear store is here to give all traders something they’ll love.</p>
 					<div className={styles['coming-soon']}>
 						<a href='' onClick={(event) => event.preventDefault()} rel='noreferrer noopener'>
