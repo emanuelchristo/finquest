4657873d3d1b38ef0ef50ec091de4404053b9fe5
salmanpary <salmanpary@gmail.com>
2022-07-17 01:35:01 +0530

    course page responsive

--- components/Course/CourseFee/CourseFee.js
+++ components/Course/CourseFee/CourseFee.js
@@ -7,7 +7,7 @@ const CourseFee = () => {
     <>
       <div className="margin">
         <div className="bg-lightblue rounded-3xl  p-16 lg:p-8 relative ">
-          <div className=" grid grid-cols-2 grid-rows-2 justify-center items-center md:grid-cols-1 md:grid-rows-3">
+          <div className=" grid grid-cols-2 grid-rows-2 justify-center items-center md:grid-cols-1 md:grid-rows-3 md:space-y-3 sm:p-3">
             <div className="row-span-2 md:flex md:flex-col md:justify-center md:items-center">
               <h2>Course Fee</h2>
               <p>Tortor,ac enim aliquuam nibh tempus</p>
@@ -18,20 +18,22 @@ const CourseFee = () => {
                 TextColor="black"
               />
             </div>
-            <div className="grid grid-cols-2 lg:grid-cols-1 md:grid-cols-2 ">
+            <div className="grid grid-cols-2 md:grid-cols-2 md:text-center">
               <div>
                 <h3 className="font-semibold text-2xl">4 Months</h3>
                 <p>Duration</p>
               </div>
               <div>
-                <h3 className="font-semibold text-2xl whitespace-nowrap">EMI Available</h3>
+                <h3 className="font-semibold text-2xl whitespace-nowrap">
+                  EMI Available
+                </h3>
                 <p>Contact us</p>
               </div>
             </div>
-            <div className="grid grid-rows-3">
+            <div className="grid grid-rows-2 md:justify-center md:items-center">
               <div className="grid grid-cols-2 lg:grid-cols-1">
-                <div className="flex">
-                  <div className="text-blue text-5xl font-bold leading-10">
+                <div className="flex md:justify-center md:items-center">
+                  <div className="text-blue text-5xl font-bold leading-10 ">
                     &#8377;5999
                   </div>
                   <div className="flex flex-col items-center justify-center">
@@ -42,7 +44,7 @@ const CourseFee = () => {
                     </span>
                   </div>
                 </div>
-                <div className="flex flex-col justify-center items-start">
+                <div className="flex flex-col justify-center items-start md:items-center">
                   <img
                     src="/icons/50.png"
                     alt=""
@@ -52,9 +54,12 @@ const CourseFee = () => {
                   />
                 </div>
               </div>
-
-              <h3 className="text-4xl font-semibold">(Including GST)</h3>
-              <p>Total price</p>
+              <div>
+                <h3 className="text-4xl font-semibold whitespace-nowrap">
+                  (Including GST)
+                </h3>
+                <p className="md:text-center">Total price</p>
+              </div>
             </div>
           </div>
           <img
@@ -68,8 +73,8 @@ const CourseFee = () => {
             src="/icons/feearrow.svg"
             alt=""
             width={125}
-            height={162}
-            className="absolute bottom-0 rotate-180 left-72"
+            height="auto"
+            className="absolute bottom-0 rotate-180 left-72 lg:right-0 lg:left-auto "
           />
         </div>
       </div>
