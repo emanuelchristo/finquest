25b464d601c10807c765cf0e5dc3f69c19b8b533
n48331 <unfolks@gmail.com>
2022-08-30 12:45:09 +0530

    cource-page-fixes

--- components/Course/WhatYouWillLearn/WhatYouWillLearn.js
+++ components/Course/WhatYouWillLearn/WhatYouWillLearn.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import Button from '../common/Buttons/Button'
-import FeaturesLayout from '../common/FeatureLayout/FeaturesLayout'
+import React from "react";
+import Button from "../common/Buttons/Button";
+import FeaturesLayout from "../common/FeatureLayout/FeaturesLayout";
 const WhatYouWillLearn = () => {
-
   return (
     <>
-    <div className="margin">
-
-    <h2 className='md:text-center mb-2'>What You will Learn</h2>
-    <div className="flex">
-
-    <Button ButtonText="Curriculum" IconName="Book" BgColor="buttongreen" TextColor="white"/>
-    </div>
-<FeaturesLayout indicator="whatyouwilllearn"/>
-    </div>
+      <div className="margin">
+        <h2 style={{ margin: "3rem 0" }} className="md:text-center mb-2 ">
+          What You will Learn
+        </h2>
+        <div className="flex">
+          <Button
+            ButtonText="Curriculum"
+            IconName="Book"
+            BgColor="buttongreen"
+            TextColor="white"
+          />
+        </div>
+        <FeaturesLayout indicator="whatyouwilllearn" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default WhatYouWillLearn
+export default WhatYouWillLearn;
--- components/Course/WhoIsThisFor/WhoIsThisFor.js
+++ components/Course/WhoIsThisFor/WhoIsThisFor.js
@@ -1,39 +1,51 @@
-import React from 'react'
-import Button from '../common/Buttons/Button'
-import FeaturesLayout from '../common/FeatureLayout/FeaturesLayout'
-import { useRouter } from 'next/router'
-import CoursePageData from '../data/CoursePageData'
+import React from "react";
+import Button from "../common/Buttons/Button";
+import FeaturesLayout from "../common/FeatureLayout/FeaturesLayout";
+import { useRouter } from "next/router";
+import CoursePageData from "../data/CoursePageData";
 
 const WhoIsThisFor = () => {
   const router = useRouter();
   const { courseid } = router.query;
-  const [data,setdata]=React.useState({});
-  React.useEffect(()=>{
-    switch(courseid){
+  const [data, setdata] = React.useState({});
+  React.useEffect(() => {
+    switch (courseid) {
       case "options":
         setdata(CoursePageData.options);
         break;
       case "professional":
         setdata(CoursePageData.professional);
         break;
-       case "starter":
+      case "starter":
         setdata(CoursePageData.starter);
     }
-  },[courseid]);
-  
+  }, [courseid]);
+
   return (
     <>
-    <div className='margin'>
-<div className='flex'>
-    <h2 className='md:text-center'>Who is this for</h2>
-    <img src="/images/sparkle.png" alt=""width="70px" height="50px" className='imageclass md:hidden'/>
-
-</div>
-    <Button ButtonText={`Enroll for ${data.newprice}`} IconName="card" BgColor="buttonyellow" TextColor="black"/>
-    <FeaturesLayout indicator="whoisthisfor"/>
-    </div>
+      <div className="margin">
+        <div className="flex">
+          <h2 style={{ margin: "3rem 0" }} className="md:text-center">
+            Who is this for
+          </h2>
+          <img
+            src="/images/sparkle.png"
+            alt=""
+            width="70px"
+            height="50px"
+            className="imageclass md:hidden"
+          />
+        </div>
+        <Button
+          ButtonText={`Enroll for ${data.newprice}`}
+          IconName="card"
+          BgColor="buttonyellow"
+          TextColor="black"
+        />
+        <FeaturesLayout indicator="whoisthisfor" />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default WhoIsThisFor
+export default WhoIsThisFor;
