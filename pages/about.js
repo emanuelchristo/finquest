beb40b19b874a80878dc60e1223fe7ee50c274a8
n48331 <unfolks@gmail.com>
2022-11-03 00:03:38 +0530

    SEO

--- pages/about.js
+++ pages/about.js
@@ -1,24 +1,37 @@
-import About1 from '../components/aboutUs/About1'
-import About2 from '../components/aboutUs/About2'
-import Hiring from '../components/aboutUs/Hiring'
-import WhyUs1 from '../components/aboutUs/WhyUs1'
-import WhyUs2 from '../components/aboutUs/WhyUs2'
-import Navbar from '../components/Navbar'
-import styles from './styles.module.css'
+import About1 from "../components/aboutUs/About1";
+import About2 from "../components/aboutUs/About2";
+import Hiring from "../components/aboutUs/Hiring";
+import WhyUs1 from "../components/aboutUs/WhyUs1";
+import WhyUs2 from "../components/aboutUs/WhyUs2";
+import Navbar from "../components/Navbar";
+import styles from "./styles.module.css";
+import Script from "next/script";
 
 export default function AboutUs() {
-	return (
-		<div id='home'>
-			<div id='home' className={styles['nav-spacer']}></div>
-			<Navbar />
-			<div id='home' className={styles['about-spacer']}></div>
-			<About1 />
-			<div id='home' className={styles['about-spacer']}></div>
-			<About2 />
-			<div id='home' className={styles['about-spacer']}></div>
-			<WhyUs1 />
-			<WhyUs2 />
-			<Hiring />
-		</div>
-	)
+  return (
+    <div id="home">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-247600968-1"
+      ></Script>
+      <Script>
+        {`
+window.dataLayer = window.dataLayer || [];
+ function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+gtag('config', 'UA-247600968-1');
+`}
+      </Script>
+      <div id="home" className={styles["nav-spacer"]}></div>
+      <Navbar />
+      <div id="home" className={styles["about-spacer"]}></div>
+      <About1 />
+      <div id="home" className={styles["about-spacer"]}></div>
+      <About2 />
+      <div id="home" className={styles["about-spacer"]}></div>
+      <WhyUs1 />
+      <WhyUs2 />
+      <Hiring />
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,54 +1,59 @@
-import About from '../components/About'
-import Associated from '../components/Associated'
-import Benefits from '../components/Benefits'
-import Community from '../components/Community'
-import Consultation from '../components/Consultation'
-import Faqs from '../components/Faqs'
+import About from "../components/About";
+import Associated from "../components/Associated";
+import Benefits from "../components/Benefits";
+import Community from "../components/Community";
+import Consultation from "../components/Consultation";
+import Faqs from "../components/Faqs";
 
-import Header from '../components/Header'
-import Letstalk from '../components/Letstalk'
-import Pricing from '../components/Pricing'
-import Quote from '../components/Quote'
-import Reviews from '../components/Reviews'
-import Rocket from '../components/Rocket'
-import Store from '../components/Store'
-
-
+import Header from "../components/Header";
+import Letstalk from "../components/Letstalk";
+import Pricing from "../components/Pricing";
+import Quote from "../components/Quote";
+import Reviews from "../components/Reviews";
+import Rocket from "../components/Rocket";
+import Store from "../components/Store";
+import Script from "next/script";
 
 export default function Home() {
-	return (
-		<div id='home'>
-			<Header />
-			<Associated />
-			<div className='section-spacer'></div>
-			<About />
-			<div className='section-spacer'></div>
-			<Rocket />
-			<div className='section-spacer'></div>
-			<Benefits />
-			<div className='section-spacer'></div>
-			<Quote />
-			<div className='section-spacer'></div>
-			<Pricing />
-			<div className='section-spacer'></div>
-			<Reviews />
-			<div className='section-spacer'></div>
-			<Consultation />
-			<div className='section-spacer'></div>
-			<Store />
-			<div className='section-spacer'></div>
-			<Faqs />
-			<div className='section-spacer'></div>
+  return (
+    <div id="home">
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=UA-247600968-1"
+      ></Script>
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+        gtag('config', 'UA-247600968-1');
+        `}
+      </Script>
+      <Header />
+      <Associated />
+      <div className="section-spacer"></div>
+      <About />
+      <div className="section-spacer"></div>
+      <Rocket />
+      <div className="section-spacer"></div>
+      <Benefits />
+      <div className="section-spacer"></div>
+      <Quote />
+      <div className="section-spacer"></div>
+      <Pricing />
+      <div className="section-spacer"></div>
+      <Reviews />
+      <div className="section-spacer"></div>
+      <Consultation />
+      <div className="section-spacer"></div>
+      <Store />
+      <div className="section-spacer"></div>
+      <Faqs />
+      <div className="section-spacer"></div>
 
-			<Letstalk />
-			<div className='section-spacer'></div>
-			<Community />
-
-		</div>
-	)
+      <Letstalk />
+      <div className="section-spacer"></div>
+      <Community />
+    </div>
+  );
 }
-
-
-
-
-
