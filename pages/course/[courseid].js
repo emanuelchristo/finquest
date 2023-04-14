d17dbbce4622fd8220ab2e32254a43d713a0dd4b
n48331 <unfolks@gmail.com>
2023-04-14 23:32:56 +0530

    key

--- pages/course/[courseid].js
+++ pages/course/[courseid].js
@@ -11,3 +11,15 @@ const Course = () => {
 //   return { props: { data } }
 // }
 export default Course;
+
+
+export async function getServerSideProps() {
+  const req = await fetch('http://localhost:3000/api/sheet');
+  const res = await req.json();
+
+  return {
+    props: {
+      sheetdata: res.data
+    }
+  }
+}
