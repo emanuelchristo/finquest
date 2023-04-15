3a0cac2d2c8fc8fed8cbcfa469d40b46104d2d69
n48331 <unfolks@gmail.com>
2023-04-16 00:28:32 +0530

    Google Tag Manager

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,6 @@ import Footer from "../components/Footer";
 import Script from "next/script";
 function MyApp({ Component, pageProps }) {
   return (
-
     <div>
       <Head>
         <title>
@@ -19,7 +18,7 @@ function MyApp({ Component, pageProps }) {
         <meta
           name="google-site-verification"
           content="4Cbyhz8hc0taFGG6F4lucI7JMMXJQe1y_Qa8qKscRnI"
-          />
+        />
       </Head>
       <Script
         id="gtm-script"
@@ -35,8 +34,11 @@ function MyApp({ Component, pageProps }) {
         `}
       </Script>
 
-
-      <Script id="pixel-code" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
+      <Script
+        id="pixel-code"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
 if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
@@ -45,8 +47,20 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '499046898871948');
-fbq('track', 'PageView');`}}></Script>
-
+fbq('track', 'PageView');`,
+        }}
+      ></Script>
+      <Script
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NQVZFL4');`,
+        }}
+      ></Script>
 
       <noscript
         dangerouslySetInnerHTML={{
@@ -57,7 +71,13 @@ fbq('track', 'PageView');`}}></Script>
           src="https://www.facebook.com/tr?id=499046898871948&ev=PageView&noscript=1"
           />`,
         }}
-        ></noscript>
+      ></noscript>
+      <noscript
+        dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NQVZFL4"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+        }}
+      ></noscript>
 
       <Component {...pageProps} />
 
